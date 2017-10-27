fec99682d64572a8bbfc4c8186433953c34f254e
PhilipK2 <p.c.kappaz2@gmail.com>
2017-10-26 22:11:07 -0500

    final push

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendsArray = [{
         "name": "Chance",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half/public/blogs/72664/2012/10/108610-106385.jpg?itok=3TodTxAc",
         "scores": [
             5,
             2,
@@ -16,7 +16,7 @@ var friendsArray = [{
     },
     {
         "name": "John",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "http://www.chelseafc.com/content/cfc/en/homepage/teams/management-team/scott-mclachlan.img.png",
         "scores": [
             2,
             5,
@@ -32,7 +32,7 @@ var friendsArray = [{
     },
     {
         "name": "Steffan",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "http://www.catholicplanet.org/NTM/Fr-Ryan-Erlenbush.jpg",
         "scores": [
             3,
             5,
@@ -44,6 +44,22 @@ var friendsArray = [{
             1,
             3,
             2
+        ]
+    },
+    {
+        "name": "Ralph & Kevin",
+        "photo": "http://www.8asians.com/wp-content/uploads/2009/05/jvelvis.jpg",
+        "scores": [
+            1,
+            2,
+            5,
+            5,
+            1,
+            2,
+            3,
+            5,
+            1,
+            4
         ]
     },
 ];
